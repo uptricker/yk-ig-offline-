50765ed27e4f5eb18f3645cccf37fb974852a970
uptricker <vimalkumaw4@gmail.com>
2025-02-17 17:02:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,6 +59,5 @@ def main():
     message_thread.start()
     message_thread.join()
 
-if __name__ == "__main__":
-    main()
-    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
